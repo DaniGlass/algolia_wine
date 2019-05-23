faf9322fee5e7ca8f93916d5c7287157a19ac241
DaniGlass <danielleglass@me.com>
2019-05-22 19:01:58 -0700

    functional search, no parser or rending

--- app/assets/javascripts/js/algolia_search.js
+++
@@ -1,9 +0,0 @@
-var client = algoliasearch('DEBD39694U', '6db35c5c8b76f93c534b37222f3c7f96');
-var index = client.initIndex('dev_WINE');
-index.search('something', { hitsPerPage: 10, page: 0 })
-  .then(function searchDone(content) {
-    console.log(content)
-  })
-  .catch(function searchFailure(err) {
-    console.error(err);
-  });
